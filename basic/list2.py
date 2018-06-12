1300745b8d5061498922761d1b808b85d12e6e4f
Fabiano da Rosa Gomes <frgomes@digicon.com.br>
2018-06-12 11:04:24 -0300

    BUG-FIX: remove_adjacent

--- basic/list2.py
+++ basic/list2.py
@@ -14,10 +14,13 @@
 # so [1, 2, 2, 3] returns [1, 2, 3]. You may create a new list or
 # modify the passed in list.
 def remove_adjacent(nums):
-    for x in nums:
-        if nums.count(x) > 1:
-            nums.remove(x)
-    return nums
+    l = []
+    if not nums:
+        return nums
+    for idx, val in enumerate(nums):
+        if nums[idx] != nums[idx-1]:
+            l.append(val)
+    return l
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
